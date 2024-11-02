00be4e3f631290e20a4ba043f8c0e1e021f794a6
root <55458737+BryanCL01@users.noreply.github.com>
2024-11-02 02:28:33 -0700

    tweaking prefab cell size

--- Assignment02/Assets/Maze/MazeRenderer.cs
+++ Assignment02/Assets/Maze/MazeRenderer.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class MazeRenderer : MonoBehaviour
 {
-    [SerializeField] MazeGenerator mazeGenerator; 
-    [SerializeField] GameObject MazeCellPrefab; 
+    [SerializeField] MazeGenerator mazeGenerator;
+    [SerializeField] GameObject MazeCellPrefab;
 
-    public float CellSize = 1f; 
+    public float CellSize = 5f;
 
-    private void Start() {
+    private void Start()
+    {
 
-        MazeCell[,] maze = mazeGenerator.GetMaze(); 
+        MazeCell[,] maze = mazeGenerator.GetMaze();
 
-        for (int x = 0; x < mazeGenerator.mazeWidth; x++) {
-            for (int y = 0; y < mazeGenerator.mazeHeight; y++) {
-                
-                GameObject newCell = Instantiate(MazeCellPrefab, new Vector3((float)x * CellSize, 0f, (float)y * CellSize), Quaternion.identity, transform); 
+        for (int x = 0; x < mazeGenerator.mazeWidth; x++)
+        {
+            for (int y = 0; y < mazeGenerator.mazeHeight; y++)
+            {
 
-                MazeCellObject mazeCell = newCell.GetComponent<MazeCellObject>(); 
+                GameObject newCell = Instantiate(MazeCellPrefab, new Vector3((float)x * CellSize, 0f, (float)y * CellSize), Quaternion.identity, transform);
 
-                bool top = maze[x, y].topWall; 
-                bool left = maze[x, y].leftWall; 
-                
-                bool botttom = false; 
-                bool right = false; 
-                if (x == mazeGenerator.mazeWidth - 1) right = true; 
-                if (y == 0) botttom = true; 
+                MazeCellObject mazeCell = newCell.GetComponent<MazeCellObject>();
 
-                mazeCell.Init(top, botttom, right, left); 
+                bool top = maze[x, y].topWall;
+                bool left = maze[x, y].leftWall;
+
+                bool botttom = false;
+                bool right = false;
+                if (x == mazeGenerator.mazeWidth - 1) right = true;
+                if (y == 0) botttom = true;
+
+                mazeCell.Init(top, botttom, right, left);
             }
         }
     }
