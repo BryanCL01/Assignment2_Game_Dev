880b5335d68dcbf756ac41f545db939727be1d75
Simran Gandhi <gandhi.k.simran@gmail.com>
2024-11-03 17:59:03 -0800

    finalizing materials

--- Assignment02/Assets/Maze/MazeCellObject.cs
+++ Assignment02/Assets/Maze/MazeCellObject.cs
@@ -8,12 +8,34 @@ public class MazeCellObject : MonoBehaviour
     [SerializeField] GameObject bottomWall; 
     [SerializeField] GameObject rightWall; 
     [SerializeField] GameObject leftWall; 
+    [SerializeField] GameObject floor; 
+
+    //For materials
+    [SerializeField] Material NorthWallMaterial;
+    [SerializeField] Material SouthWallMaterial;
+    [SerializeField] Material EastWallMaterial;
+    [SerializeField] Material WestWallMaterial;
+    [SerializeField] Material floorMaterial;
+   
 
     public void Init (bool top, bool bottom, bool right, bool left) {
         topWall.SetActive(top); 
         bottomWall.SetActive(bottom); 
         rightWall.SetActive(right); 
         leftWall.SetActive(left); 
+
+
+        if (topWall != null)
+            topWall.GetComponent<Renderer>().material = NorthWallMaterial;
+        if (bottomWall != null)
+            bottomWall.GetComponent<Renderer>().material = SouthWallMaterial;
+        if (rightWall != null)
+            rightWall.GetComponent<Renderer>().material = EastWallMaterial;
+        if (leftWall != null)
+            leftWall.GetComponent<Renderer>().material = WestWallMaterial;
+        if (floor != null)
+            floor.GetComponent<Renderer>().material = floorMaterial;
+       
 
     }
 }
