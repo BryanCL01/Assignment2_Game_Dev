a38c19207aa17bf6226a9e0ba57700b9f038892e
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2024-12-02 00:05:16 -0800

    updates

--- Assignment02/Assets/Ball/BallController.cs
+++ Assignment02/Assets/Ball/BallController.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        // Get the AudioSource component
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
         Debug.Log(collision.gameObject);
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log("Hit");
             // Update the score
             FindObjectOfType<ScoreManager>().UpdateScore();
 
             // Destroy the ball
-            gameObject.transform.position = new Vector3(0,-50, 0);
+            gameObject.transform.position = new Vector3(0, -50, 0);
 
             // Reduce enemy's health
             collision.gameObject.GetComponent<Enemy>().TakeDamage();
